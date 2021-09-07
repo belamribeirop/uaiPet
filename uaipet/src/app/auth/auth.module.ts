a0126a4dc482438b4bcdfe58347c8e1b628b9795
Isabela Ribeiro <belamribeirop@gmail.com>
2021-09-06 21:16:37 -0300

    register Ong route

--- uaipet/src/app/auth/auth.module.ts
+++ uaipet/src/app/auth/auth.module.ts
@@ -4,22 +4,26 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthComponent } from './auth/auth.component';
 import { AuthRoutingModule } from './auth-routing.module';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RegisterOngComponent } from './RegisterOng/registerong.component';
 
 @NgModule({
   declarations: [
     LoginComponent,
     RegisterComponent,
-    AuthComponent
+    AuthComponent,
+    RegisterOngComponent,
   ],
   imports: [
     CommonModule,
     AuthRoutingModule,
     MatInputModule,
     MatButtonModule,
-    ReactiveFormsModule
-  ]
+    ReactiveFormsModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
-export class AuthModule { }
+export class AuthModule {}
