6e32d088aa86df7dd44cd172c115903418125587
lucianawasner <lucianawasner@gmail.com>
2021-11-16 10:12:25 -0300

    modificacoes professor

--- uaipet/src/app/main/animal-control/animal-control.component.ts
+++ uaipet/src/app/main/animal-control/animal-control.component.ts
@@ -7,7 +7,9 @@ import {
 import { CdkTableModule, DataSource } from '@angular/cdk/table';
 import { MatTableModule } from '@angular/material/table';
 import { ControlDialogComponent } from './control-dialog/control-dialog.component';
-import { HttpClient } from '@angular/common/http';
+
+// Aqui! import { HttpClient } from '@angular/common/http';
+
 import { AnimalsService } from '../Services/Animals/animals.service';
 
 export interface MyAnimals {
@@ -19,7 +21,63 @@ export interface MyAnimals {
   adopted: boolean;
   category: string;
   raca: string;
+};
+
+
+export interface AnimalsData {
+  id: number;
+  name: string;
+  category: string;
+  gender: string;
+};
+
+/*
+{
+  "id": 1,
+  "name": "fsdf",
+  "color": "vdfb",
+  "age": "dfv",
+  "size": "garnde",
+  "gender": "fem",
+  "adopted": false,
+  "category": "bfgbsf",
+  "raca": "gfbb",
+
+  "caracteristicas": "car1",
+  "health": "saudavel",
+  "adestrado": true,
 }
+*/
+
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
+
+const ANIMAL_DATA: AnimalsData[] = [
+  {id: 1, name: 'Hydrogen', category: 'teste', gender: 'H'},
+  {id: 2, name: 'Helium', category: 'teste', gender: 'He'},
+  {id: 3, name: 'Lithium', category: 'teste', gender: 'Li'},
+];
+
+
 
 @Component({
   selector: 'app-animal-control',
@@ -27,18 +85,36 @@ export interface MyAnimals {
   styleUrls: ['./animal-control.component.scss'],
 })
 export class AnimalControlComponent implements OnInit {
+
+  // displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  // dataSource = ELEMENT_DATA;
+
+  displayedColumns: string[] = ['id', 'name', 'category', 'gender'];
+  dataSource: AnimalsData[] = []; // dataSource = ANIMAL_DATA;
+
   constructor(
     public dialog: MatDialog,
     private animalsService: AnimalsService
   ) {}
-  dataSource: MyAnimals[];
+
   ngOnInit(): void {
+
     this.animalsService.getAll().subscribe((data) => {
-      this.dataSource = data;
+      // Aqui!
+     this.dataSource = data.map((item, index)=>{
+        return {
+          id: index,
+          name: item.name,
+          category: item.category,
+          gender: item.gender
+        }
+      });
+
+      console.log(this.dataSource);
     });
   }
 
-  displayedColumns: string[] = ['name', 'category', 'adopted', 'actions'];
+
 
   addAnimal() {
     console.log('addAnimal');
--- uaipet/src/app/main/animals/animals.component.ts
+++ uaipet/src/app/main/animals/animals.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 type Animal = {
   id: string;
   name: string;
@@ -18,8 +19,19 @@ export class AnimalsComponent implements OnInit {
   allAnimals: Animal[] = [];
   animals: Animal[] = [];
   filters = ['cachorro', 'gato', 'filhote', 'adulto'];
-  selectedAnimals: Animal;
+
+  // Aqui! selectedAnimals: Animal;
+  selectedAnimals: Animal = {
+    id: '0',
+    name: 'bob',
+    age: 'filhote',
+    type: 'cachorro',
+    category: 'vira lata',
+    image: 'https://static.vecteezy.com/ti/fotos-gratis/p2/963511-filhote-de-cachorro-poodle-branco-bonito-foto.jpg'
+  };
+
   ngOnInit(): void {
+
     this.allAnimals = [
       {
         id: '0',
@@ -27,8 +39,8 @@ export class AnimalsComponent implements OnInit {
         age: 'filhote',
         type: 'cachorro',
         category: 'vira lata',
-        image:
-          'https://cidadeverde.com/assets/uploads/posts/e769b0d28c90b17e276c0c3423de0a2e.jpg',
+        image: 'https://static.vecteezy.com/ti/fotos-gratis/p2/963511-filhote-de-cachorro-poodle-branco-bonito-foto.jpg',
+          // 'https://cidadeverde.com/assets/uploads/posts/e769b0d28c90b17e276c0c3423de0a2e.jpg',
       },
       {
         id: '1',
@@ -58,6 +70,7 @@ export class AnimalsComponent implements OnInit {
           'https://blog.cobasi.com.br/wp-content/uploads/2020/11/pastor-alemao-filhote-studio.png',
       },
     ];
+
     this.animals = [...this.allAnimals];
   }
   openDetails(id: string) {
